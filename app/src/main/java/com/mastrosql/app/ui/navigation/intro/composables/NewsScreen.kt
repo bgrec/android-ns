3e57094a9831177926443af6498ab82552c96b82
PileggiThomas <thomas@nipeservice.com>
2024-02-29 14:17:08 +0100

    working on news

--- app/src/main/java/com/mastrosql/app/ui/navigation/intro/composables/NewsScreen.kt
+++ app/src/main/java/com/mastrosql/app/ui/navigation/intro/composables/NewsScreen.kt
@@ -1,10 +1,38 @@
 package com.mastrosql.app.ui.navigation.intro.composables
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.navigation.NavController
+import com.mastrosql.app.R
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun NewsScreen(navController: NavController){
+fun NewsScreen(navController: NavController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(R.string.news_title),
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                    )
+                })
+        }){
+        Column (
+            modifier = Modifier.padding(it)
+        ){
 
+        }
+    }
 }
